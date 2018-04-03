357a573f3131847cac599a2b9ee5fdc74ca4788a
Adam Yen <ayen@tibco.com>
2018-04-03 01:00:57 -0400

    add CF_INSTANCE_INDEX on nodename for clustering

--- resources/startsb_cf.sh
+++ resources/startsb_cf.sh
@@ -41,7 +41,7 @@ fi
 
 # check if nodename is present
 if [ ! -z "$NODENAME" ]; then
-    NODE_NAME="nodename=$NODENAME"
+    NODE_NAME="nodename=$NODENAME${CF_INSTANCE_INDEX}"
 else
   if [ -z "$DOMAIN_NAME" ]; then
     NODENAME=$HOSTNAME.$ClusterName
